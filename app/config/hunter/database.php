18592efc239b423b5dfc59dd84dc6f4929d3d92b
Liam Ma <liam.q.ma@gmail.com>
2014-04-21 11:25:22 +1000

    Change prod database name

--- app/config/hunter/database.php
+++ app/config/hunter/database.php
@@ -23,7 +23,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'birdieland',
 			'username'  => 'root',
 			'password'  => getenv('DB_PASSWORD'),
 			'charset'   => 'utf8',
