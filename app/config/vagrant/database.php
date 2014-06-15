259c58789c334d43310cddb3a816efa125b4b87d
Liam Ma <liam.q.ma@gmail.com>
2014-06-15 20:21:38 +1000

    Use Homestead

---
+++ app/config/vagrant/database.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+
+
+	'default' => 'mysql',
+
+	'connections' => array(
+
+
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'birdieland',
+			'username'  => 'homestead',
+			'password'  => 'secret',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		),
+
+
+	),
+
+
+);
--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,6 +27,7 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local' => array('Liam-PC'),
+	'vagrant' => array('homestead'),
 	'work' => array('LIAM-PC'),
 	'hunter' => array('hunter')
 
