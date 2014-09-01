1ebcb13b07095913a1e48b3c4d96db5be00ad015
Thomas Sørensen <ts@jumdum.com>
2014-09-01 22:13:17 +0200

    tab indentation again..

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -4,20 +4,20 @@ return array(
 
 
 	'default' => 'mysql',
+	
+	'connections' => array(
 
-    'connections' => array(
-
-        'mysql' => array(
-            'driver'    => 'mysql',
-            'host'      => $_ENV['DB_HOST'],
-            'database'  => $_ENV['DB_DATABASE'],
-            'username'  => $_ENV['DB_USERNAME'],
-            'password'  => $_ENV['DB_PASSWORD'],
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-        ),
-    )
+		'mysql' => array(
+			'driver' => 'mysql',
+			'host' => $_ENV['DB_HOST'],
+			'database' => $_ENV['DB_DATABASE'],
+			'username' => $_ENV['DB_USERNAME'],
+			'password' => $_ENV['DB_PASSWORD'],
+			'charset' => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix' => '',
+		),
+	)
 
 
 );
